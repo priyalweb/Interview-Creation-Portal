922a474ab4221e204910eab596b1a3f17024fc1e
Priyal Dubey <priyaldubey6863@gmail.com>
2021-12-18 20:44:53 +0530

    adding sql file

---
+++ interviewall.sql
@@ -0,0 +1,204 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 18, 2021 at 04:14 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `interviewall`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interview`
+--
+
+CREATE TABLE `interview` (
+  `interview_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `profile_id` int(11) NOT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `start` time DEFAULT NULL,
+  `end` time DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `headline` text DEFAULT NULL,
+  `summary` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `interview`
+--
+
+INSERT INTO `interview` (`interview_id`, `user_id`, `profile_id`, `rank`, `start`, `end`, `date`, `headline`, `summary`) VALUES
+(37, 2, 5, 1, '10:00:00', '11:00:00', '2021-12-20', 'SDE Interview ', 'Technical Round 1'),
+(38, 2, 8, 2, '10:00:00', '11:00:00', '2021-12-20', 'SDE Interview ', 'Technical Round 1'),
+(39, 2, 2, 3, '10:00:00', '11:00:00', '2021-12-20', 'SDE Interview ', 'Technical Round 1'),
+(40, 2, 3, 1, '16:30:00', '17:30:00', '2021-12-23', 'Content Review Interview', 'Tech + Hr Round'),
+(41, 2, 6, 2, '16:30:00', '17:30:00', '2021-12-23', 'Content Review Interview', 'Tech + Hr Round'),
+(42, 2, 2, 1, '15:30:00', '16:30:00', '2021-12-25', 'HP interview', 'Round 3'),
+(43, 2, 5, 2, '15:30:00', '16:30:00', '2021-12-25', 'HP interview', 'Round 3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meeting`
+--
+
+CREATE TABLE `meeting` (
+  `meeting_id` int(11) NOT NULL,
+  `interview_id` int(11) DEFAULT NULL,
+  `profile_id` int(11) DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `meeting`
+--
+
+INSERT INTO `meeting` (`meeting_id`, `interview_id`, `profile_id`, `rank`) VALUES
+(22, 37, 5, 1),
+(23, 38, 8, 2),
+(24, 39, 2, 3),
+(25, 40, 3, 1),
+(26, 41, 6, 2),
+(27, 42, 2, 1),
+(28, 43, 5, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `profile_id` int(11) NOT NULL,
+  `name` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`profile_id`, `name`) VALUES
+(2, 'Kriti'),
+(3, 'Hailey'),
+(5, 'Rachna'),
+(6, 'Sanya'),
+(8, 'Danish'),
+(9, 'Tripti');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(128) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `email`, `password`) VALUES
+(2, 'admin', 'cooladmin@gmail.com', '83699e97a7dfb2636fee4c0c12bff008');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `interview`
+--
+ALTER TABLE `interview`
+  ADD PRIMARY KEY (`interview_id`,`profile_id`) USING BTREE,
+  ADD KEY `interview_ibfk_1` (`profile_id`);
+
+--
+-- Indexes for table `meeting`
+--
+ALTER TABLE `meeting`
+  ADD PRIMARY KEY (`meeting_id`),
+  ADD KEY `meeting_ibfk_1` (`profile_id`),
+  ADD KEY `meeting_ibfk_2` (`interview_id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`profile_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `interview`
+--
+ALTER TABLE `interview`
+  MODIFY `interview_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT for table `meeting`
+--
+ALTER TABLE `meeting`
+  MODIFY `meeting_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `interview`
+--
+ALTER TABLE `interview`
+  ADD CONSTRAINT `interview_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `meeting`
+--
+ALTER TABLE `meeting`
+  ADD CONSTRAINT `meeting_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `meeting_ibfk_2` FOREIGN KEY (`interview_id`) REFERENCES `interview` (`interview_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
