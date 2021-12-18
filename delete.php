0d80b2e76a42cc6ddd1eaa37653a8fe50729e587
Priyal Dubey <56549165+priyalweb@users.noreply.github.com>
2021-12-18 22:06:07 +0530

    Delete an existing interview

--- delete.php
+++ delete.php
@@ -17,6 +17,7 @@ if ( isset($_POST['delete']) && isset($_POST['interview_id'])
     header( 'Location: index.php' ) ;
     return;
 }
+
 if (isset($_POST['cancel'])){
   header( 'Location: index.php' ) ;
   return;
